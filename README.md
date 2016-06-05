c756cae07be5155c2213f825e43217c49ee612e0
Aamir Kapasi <aamir@helpshift.com>
2016-06-05 11:34:07 +0530

    Reorder pinned tabs
    - Fix moving an unpinned tab to the start if there are pinned tabs
    - Update readme

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # tabz-4-dayz
 A chrome extension to manage your tabs with keyboard shortcuts!
 
-## Features 
-- Switch to left/right tab using `ctrl + j` & `ctrl + l`
+## Features
+- Switch to the left and right tabs using `ctrl + j` & `ctrl + l`
 - Toggle back & forth between last visited tabs using `cmd/alt + j` & `cmd/alt + shift + j`
-- Reorder tab to the left/right or start/end
+- Shortcuts for reordering pinned and unpinned tabs
 
 ## Todo
-- Fix move tab to start when tabs are pinned
-- Suspend tab
 - Pin/unpin tabs
+- Suspend tab
 - Fuzzy search through tabs
--- src/tab-move.js
+++ src/tab-move.js
@@ -1,35 +1,49 @@
 var tabs = require ("./tab-utils.js");
 
-// Creates a function that will move the current tab to a new index
-// nextIndex can be either a number or a function that returns a number
-var createMoveFn = function (nextIndex) {
+/*
+ * Creates a function that will move the current tab to a new index.
+ * Expects a function that will return the new index. The function will receive
+ * the active tab and an array of the tabs in the current window as args.
+ */
+var createMoveFn = function (newIndexFn) {
   return function () {
-    tabs.getCurrent (function (activeTab) {
-      var index;
-      if (typeof nextIndex === "function") {
-        index = nextIndex (activeTab.index);
-      } else {
-        index = nextIndex;
-      }
-
-      chrome.tabs.move (activeTab.id, {index: index});
+    tabs.getCurrent (function (activeTab, _tabs) {
+      chrome.tabs.move (activeTab.id, {
+        index: newIndexFn (activeTab, _tabs)
+      });
     });
-  }
+  };
 };
 
-var right = createMoveFn (function (currentIndex) {
-  return currentIndex + 1;
+var right = createMoveFn (function (activeTab) {
+  return activeTab.index + 1;
 });
 
-var left = createMoveFn (function (currentIndex) {
-  // Chrome allows negative indices so to avoid moving the tab to the other end
-  // the minimum index allowed is 0
+var left = createMoveFn (function (activeTab) {
+  var currentIndex = activeTab.index;
+  // Chrome allows negative indices so to avoid moving the tab to the end the
+  // minimum index allowed is 0
   return currentIndex === 0 ? 0 : currentIndex - 1;
 });
 
-var start = createMoveFn (0);
+var start = createMoveFn (function (activeTab, _tabs) {
+  if (activeTab.pinned) {
+    // If the active tab is pinned then blindly move it to the first index
+    return 0;
+  } else {
+    // If the active tab is unpinned find the first unpinned tab's index
+    return _tabs.find (tab => !tab.pinned).index;
+  }
+});
 
-var end = createMoveFn (-1);
+var end = createMoveFn (function (activeTab, _tabs) {
+  if (activeTab.pinned) {
+    // If the active tab is pinned find the last pinned tab's index
+    return _tabs.filter (tab => tab.pinned).length - 1;
+  } else {
+    return -1;
+  }
+});
 
 module.exports = {
   right: right,
